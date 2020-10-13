bd50a78350022011bc21161871f1505e804a8481
s18002 <s18002@std.it-college.ac.jp>
2020-10-13 16:34:55 +0900

    画面遷移完了

--- app/src/main/java/com/example/altesuto/MainActivity.kt
+++ app/src/main/java/com/example/altesuto/MainActivity.kt
@@ -2,7 +2,22 @@ package com.example.altesuto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import kotlinx.android.synthetic.main.activity_main.*
 
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        sendButton.setOnClickListener {
+            val intent = Intent(application, SubActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
+/*
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,3 +25,4 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
+ */
---
+++ app/src/main/java/com/example/altesuto/SubActivity.kt
@@ -0,0 +1,17 @@
+package com.example.altesuto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_sub.*
+
+class SubActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sub)
+
+        returnButton.setOnClickListener {
+            finish()
+        }
+    }
+}
